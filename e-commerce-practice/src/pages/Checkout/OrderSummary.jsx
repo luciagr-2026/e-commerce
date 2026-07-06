bf927e1537352f5651bc777a17440eb9df9fe9af
luciagr-2026 <luciagroldan@alumnos.cei.es>
2026-07-06 23:28:27 +0200

    Create update delivery option feature

--- e-commerce-practice/src/pages/Checkout/OrderSummary.jsx
+++ e-commerce-practice/src/pages/Checkout/OrderSummary.jsx
@@ -2,7 +2,7 @@ import { DeliveryOptions } from "../Checkout/DeliveryOptions";
 import { DeliveryDate } from "../Checkout/DeliveryDate";
 import { CartItemDetails } from "../Checkout/CartItemDetails";
 
-export function OrderSummary({ cart, deliveryOptions }) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -22,6 +22,7 @@ export function OrderSummary({ cart, deliveryOptions }) {
               <DeliveryOptions
                 cartItem={cartItem}
                 deliveryOptions={deliveryOptions}
+                loadCart={loadCart}
               />
             </div>
           </div>
